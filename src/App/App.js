95dc4596161d9b135ea0ec26d33a62d59047e81e
Steve Rumizen <steverumizen@gmail.com>
2019-08-22 15:02:49 -0600

    Add returnRandomHex, returnColors and createScheme methods
    Co-Authored-By: Andreea Hanson <andreea.hanson7@gmail.com>

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import Sidebar from '../Sidebar/Sidebar';
 // import ColorContainer from '../ColorContainer/ColorContainer';
-import { fetchProjects } from '../apiCalls';
-import './App.scss';
+import { fetchProjects } from "../apiCalls";
+import "./App.scss";
+const ColorScheme = require("color-scheme");
 
 class App extends Component {
   state = {
     projects: []
-  }
+  };
 
   async componentDidMount() {
-    const projects = await fetchProjects('http://swatchr-be.herokuapp.com/api/v1/projects');
-    this.setState({ projects })
-    console.log(this.state.projects)
-    // set state with projects
+    const projects = await fetchProjects(
+      "http://swatchr-be.herokuapp.com/api/v1/projects"
+    );
+    this.setState({ projects });
   }
 
   returnProjectWithPalettes = async id => {
@@ -21,20 +22,37 @@ class App extends Component {
       `http://swatchr-be.herokuapp.com/api/v1/projects/${id}`
     );
     return projectWithPalettes;
+  };
+
+  returnColors = colors => {
+    return colors;
+  };
+
+  returnRandomHex = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  createScheme = () => {
+    let scheme = new ColorScheme();
+    const randomColor = this.returnRandomHex()
+    scheme.from_hex(randomColor); 
+    let colors = [ ...scheme.colors(), randomColor];
+    return colors;
   }
 
-  returnColors = (colors) => {
-    //return colors
-  }
-
-  render(){
+  render() {
     return (
       <>
-      <h1>App</h1>
-      {/* <ColorContainer colors={this.returnColors()}/> */}
-      {/* <Sidebar projects={this.state.projects} returnColors={this.returnColors}/> */}
+        <h1>App</h1>
+        {/* <ColorContainer colors={this.returnColors()}/> */}
+        {/* <Sidebar projects={this.state.projects} returnColors={this.returnColors}/> */}
       </>
-    )
+    );
   }
 }
 
