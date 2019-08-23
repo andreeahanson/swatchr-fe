27d7ff29f4ac4b3a1922321c189a1e803dc9002f
Andreea Hanson <andreea.hanson7@gmail.com>
2019-08-23 15:24:51 -0600

    Add sidebar sliding animation
    Co-Authored-By: Steve Rumizen <steverumizen@gmail.com>

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -10,14 +10,22 @@ class Sidebar extends Component {
     projectSelect: "",
     addPalette: false,
     paletteName: "",
-    currentProject: {}
+    currentProject: {},
+    navDisplay: false
   };
 
   displayPalettes = () => {
     return this.state.currentProject.palettes.map((palette, i) => {
       const { color1, color2, color3, color4, color5, name } = palette;
       const colors = [color1, color2, color3, color4, color5];
-      return <Palette key={i} colors={colors} name={name} returnColors={this.props.returnColors} />;
+      return (
+        <Palette
+          key={i}
+          colors={colors}
+          name={name}
+          returnColors={this.props.returnColors}
+        />
+      );
     });
   };
 
@@ -29,13 +37,13 @@ class Sidebar extends Component {
       </option>
     ));
   };
-  
+
   selectProject = async e => {
     const projectId = parseInt(e.nativeEvent.target.selectedOptions[0].id);
     const projectValue = e.nativeEvent.target.selectedOptions[0].value;
     let currentProject;
-    if(projectId === 0) {
-      currentProject = {}
+    if (projectId === 0) {
+      currentProject = {};
     } else {
       currentProject = await this.props.returnProjectWithPalettes(projectId);
     }
@@ -58,50 +66,70 @@ class Sidebar extends Component {
     }
   };
 
+  toggleNav = () => {
+    this.setState({navDisplay : !this.state.navDisplay})
+  }
+
   render() {
     return (
-      <nav className="sidebar">
-        <form className="projects-form">
-          <select
-            className="projects-select"
-            onChange={this.selectProject}
-            value={this.state.selectedProject}
-          >
-            <option
-            value='Select Project'
-            id={0}
-            >Select Project</option>
-            {this.props.projects.length && this.displayOptions()}
-          </select>
-          <button name="projectBtn" className="add-proj-btn" onClick={this.toggleInput}>
-            + Add Project
-          </button>
-          {this.state.addProject && (
-            <input
-              name="projectName"
-              type="text"
-              value={this.state.projectName}
-              onChange={this.handleChange}
-              className="add-proj-input"
-            />
-          )}
-        </form>
-        <form className="palettes-form">
-          <button name="paletteBtn" className="add-palette-btn" onClick={this.toggleInput}>
-            + Save palette
-          </button>
-          {this.state.addPalette && (
-            <input
-              name="paletteName"
-              type="text"
-              value={this.state.paletteName}
-              onChange={this.handleChange}
-              className="add-palette-input"
-            />
-          )}
-        </form>
-        <section className="palettes-section">{this.state.currentProject.palettes && this.displayPalettes()}</section>
-      </nav>
+      <div className="sidebar-wrapper">
+        <div className="sidebar-arrow" onClick={this.toggleNav}>
+          {!this.state.navDisplay && <img src="./left.png" alt="left-caret"/>}
+          {this.state.navDisplay && <img src="./right.png" alt="right-caret"/>}
+        </div>
+        {this.state.navDisplay && 
+        <nav className="sidebar">
+          <form className="projects-form">
+            <select
+              className="projects-select"
+              onChange={this.selectProject}
+              value={this.state.selectedProject}
+            >
+              <option value="Select Project" id={0}>
+                Select Project
+              </option>
+              {this.props.projects.length && this.displayOptions()}
+            </select>
+            <button
+              name="projectBtn"
+              className="add-proj-btn"
+              onClick={this.toggleInput}
+            >
+              + Add Project
+            </button>
+            {this.state.addProject && (
+              <input
+                name="projectName"
+                type="text"
+                value={this.state.projectName}
+                onChange={this.handleChange}
+                className="add-proj-input"
+              />
+            )}
+          </form>
+          <form className="palettes-form">
+            <button
+              name="paletteBtn"
+              className="add-palette-btn"
+              onClick={this.toggleInput}
+            >
+              + Save palette
+            </button>
+            {this.state.addPalette && (
+              <input
+                name="paletteName"
+                type="text"
+                value={this.state.paletteName}
+                onChange={this.handleChange}
+                className="add-palette-input"
+              />
+            )}
+          </form>
+          <section className="palettes-section">
+            {this.state.currentProject.palettes && this.displayPalettes()}
+          </section>
+        </nav>}
+      </div>
     );
   }
 }
