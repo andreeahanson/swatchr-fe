d803487cdd4b7ed8bdf399fe56dd53e80f187e19
Andreea Hanson <andreea.hanson7@gmail.com>
2019-08-25 15:22:54 -0600

    Add happy and sad path tests for fetchOnePalette

--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -153,5 +153,49 @@ describe("apiCalls", () => {
     });
   });
 
+  describe("fetchOnePalette", () => {
+    let mockPalette;
+
+    beforeEach(() => {
+      mockPalette = {
+        name: "Palette 1",
+        color1: "#B06454",
+        color2: "#B7AE23",
+        color3: "#39B723",
+        color4: "#23B7B7",
+        color5: "#232EB7"
+      };
+
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockPalette)
+        });
+      });
+    });
+
+    it.skip("should be called with correct URL", () => {
+      const expected = "http://swatchr-be.herokuapp.com/api/v1/projects/1/palettes";
+      fetchOnePalette();
+      expect(window.fetch).toHaveBeenCalledWith(expected);
+    });
+
+    it("HAPPY: should return with a parsed response", async () => {
+      const result = await fetchOnePalette();
+      expect(result).toEqual(mockPalette);
+    });
+
+    it("SAD: should return an error if the answer is not ok", () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      expect(fetchOnePalette()).rejects.toEqual(
+        Error("Could not fetch palette")
+      );
+    });
+  });
+
 
 });
