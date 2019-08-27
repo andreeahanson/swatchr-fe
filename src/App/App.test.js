7d392402addefa521759e05812d3b44d93c506bd
Andreea Hanson <andreea.hanson7@gmail.com>
2019-08-26 23:26:07 -0600

    Add test for cleanProjects

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -11,5 +11,17 @@ describe('App', () => {
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should return projects with id and name as properties when cleanProjects is invoked', () => {
+    const projects = [{ id: 1, name: "Pojr", created_at: "9999" }]
+
+    const cleanProjects = [{ id: 1, name: "Pojr" }]
+
+    const result = [{ id: 1, name: "Pojr" } ]
+
+    wrapper.instance().cleanProjects(projects);
+
+    expect(result).toEqual(cleanProjects);
   })
 })
