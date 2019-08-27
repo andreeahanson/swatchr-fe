4d63b0bbb3e8fdc82b6af37990b380cb6fab02c9
Steve Rumizen <steverumizen@gmail.com>
2019-08-27 14:43:06 -0600

    Add mapLockedColors test
    Co-Authored-By: Andreea Hanson <andreea.hanson7@gmail.com>

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import App from './App';
-import { shallow } from 'enzyme';
+import React from "react";
+import App from "./App";
+import { shallow } from "enzyme";
 
-describe('App', () => {
+describe("App", () => {
   let wrapper;
   let instance;
 
   beforeEach(() => {
-    wrapper = shallow(<App />)
+    wrapper = shallow(<App />);
     instance = wrapper.instance();
-  })
+  });
 
-  it('should match the snapshot', () => {
+  it("should match the snapshot", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should return projects with id and name as properties when cleanProjects is invoked', () => {
-    const projects = [{ id: 1, name: "Pojr", created_at: "9999" }]
+  it("should return projects with id and name as properties when cleanProjects is invoked", () => {
+    const projects = [{ id: 1, name: "Pojr", created_at: "9999" }];
 
-    const cleanProjects = [{ id: 1, name: "Pojr" }]
+    const cleanProjects = [{ id: 1, name: "Pojr" }];
 
-    const result = [{ id: 1, name: "Pojr" } ]
+    const result = [{ id: 1, name: "Pojr" }];
 
     wrapper.instance().cleanProjects(projects);
 
@@ -47,4 +47,94 @@ describe('App', () => {
 
     expect(instance.createScheme).toHaveBeenCalled();
   });
-})
+
+  it("should return an array of color objects when mapLockedColors is invoked", () => {
+    instance.state.colors = [
+      {
+        hex: "ffffff",
+        locked: false,
+        id: "4aad0620-c90a-11e9-904f-d561aba1162f"
+      },
+      {
+        hex: "8B663F",
+        locked: true,
+        id: "fake2"
+      },
+      {
+        hex: "FFEEDC",
+        locked: true,
+        id: "fake1"
+      },
+      {
+        hex: "cccccc",
+        locked: false,
+        id: "4aad0623-c90a-11e9-904f-d561aba1162f"
+      },
+      {
+        hex: "dddddd",
+        locked: false,
+        id: "4aad0624-c90a-11e9-904f-d561aba1162f"
+      }
+    ];
+
+    const mockNewColors = [
+      {
+        hex: "C7925A",
+        locked: false,
+        id: "4aad0620-c90a-11e9-904f-d561aba1162f"
+      },
+      {
+        hex: "8B663F",
+        locked: false,
+        id: "fake2"
+      },
+      {
+        hex: "FFEEDC",
+        locked: false,
+        id: "fake1"
+      },
+      {
+        hex: "FFDDB9",
+        locked: false,
+        id: "4aad0623-c90a-11e9-904f-d561aba1162f"
+      },
+      {
+        hex: "C78A5A",
+        locked: false,
+        id: "4aad0624-c90a-11e9-904f-d561aba1162f"
+      }
+    ];
+
+    const colors = instance.mapLockedColors(mockNewColors);
+
+    const expectedColors = [
+      {
+        hex: "C7925A",
+        locked: false,
+        id: "4aad0620-c90a-11e9-904f-d561aba1162f"
+      },
+      {
+        hex: "8B663F",
+        locked: true,
+        id: "fake2"
+      },
+      {
+        hex: "FFEEDC",
+        locked: true,
+        id: "fake1"
+      },
+      {
+        hex: "FFDDB9",
+        locked: false,
+        id: "4aad0623-c90a-11e9-904f-d561aba1162f"
+      },
+      {
+        hex: "C78A5A",
+        locked: false,
+        id: "4aad0624-c90a-11e9-904f-d561aba1162f"
+      }
+    ];
+
+    expect(colors).toEqual(expectedColors);
+  });
+});
